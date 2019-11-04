183d329c2209f02b9a1bfea9cfcb98d80f6d2d86
mejiacampos <marvin.campos2000@gmail.com>
2019-11-04 01:14:09 +0100

    modificando la clase MainActivity

--- app/src/main/java/com/example/crudmy2019/MainActivity.java
+++ app/src/main/java/com/example/crudmy2019/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.crudmy2019;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText et_codigo, et_descripcion, et_precio;
+    private Button btn_guardar, btn_consultacodigo, btn_consultaDescripcion, btn_eliminar, btn_actualizar;
+
+    boolean inputEt=false;
+    boolean inputEd=false;
+    boolean input1=false;
+
+    String senal = "";
+    String codigo = "";
+    String descripcion = "";
+    String precio = "";
+
+    MantenimientoMySQL manto = new MantenimientoMySQL();
+    Dto datos = new Dto;
+
+    boolean estadoGuarda = false;
+    Boolean estadoEliminar = false;
+
+    AlertDialog.Builder.dialogo;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected boolean onKeyDwn(int KeyCode, KeyEvent) {
+       if (KeyCode == KeyEvent.KEYCODE_BACK) {
+           new android.app.AlertDialog.Builder(this)
+                   .setIcon(R.drawable.ic_close)
+                   .setTitle("Advertencia")
+                   .setMessage("¿Realmente desea salir?")
+                   .setNegativeButton(android.R.string.cancel, null)
+                   .setPositiveButton(android.R.string.ok, {
+                       MainActivity.this.finishAffinity();
+                   })
+           .show();
+           return true;
+       }
+       return super.onKeyDown(KeyCode, event);
     }
 }
