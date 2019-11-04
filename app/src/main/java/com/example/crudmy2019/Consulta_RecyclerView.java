d92190a775a8bfd4803dc107da6e2e77160d91fd
mejiacampos <marvin.campos2000@gmail.com>
2019-11-04 02:41:15 +0100

    modificando mas la clase Consulta_RecyclerView

--- app/src/main/java/com/example/crudmy2019/Consulta_RecyclerView.java
+++ app/src/main/java/com/example/crudmy2019/Consulta_RecyclerView.java
@@ -1,4 +1,161 @@
 package com.example.crudmy2019;
 
-public class Consulta_RecyclerView {
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Consulta_RecyclerView extends AppCompatActivity {
+
+    private static final String URL = Config.urlConsultaApiMySQLi;
+
+    List<Productos> productosList;
+    RecyclerView recyclerView;
+
+    ProductsAdapter adapter;
+
+    AlertDialog.Builder dialogo;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_close)
+                    .setTitle("Advertencia")
+                    .setMessage("¿Realmente desea salir?")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta__recycler_view);
+
+        Toolbar toolbar = findViewById(R.id.toolbar1);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_back));
+        toolbar.setTitleTextColor(getResources().getColor(R.color.mycolor1));
+        toolbar.setTitleMargin(0, 0, 0, 0);
+        toolbar.setSubtitle("Consulta de Artículos");
+        toolbar.setSubtitleTextColor(getResources().getColor(R.color.mycolor));
+        toolbar.setTitle("Prof. Gámez");
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogConfirmacion();
+            }
+        });
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        productosList = new ArrayList<>();
+
+        loadProductos();
+
+    }
+
+
+    private void loadProductos() {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray array = new JSONArray(response);
+                            int totalEncontrados = array.length();
+                            Toast.makeText(Consulta_RecyclerView.this, "Total: "+totalEncontrados, Toast.LENGTH_SHORT).show();
+
+                            for (int i = 0; i < array.length(); i++) {
+
+                                JSONObject articulosObject = array.getJSONObject(i);
+                                productosList.add(new Productos(
+                                        articulosObject.getInt("codigo"),
+                                        articulosObject.getString("descripcion"),
+                                        articulosObject.getDouble("precio"),
+                                        articulosObject.getString("imagen")
+                                ));
+                            }
+
+                            adapter = new ProductsAdapter(Consulta_RecyclerView.this, productosList);
+                            recyclerView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Consulta_RecyclerView.this, "Error. Compruebe su acceso a Internet.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        MySingleton.getInstance(Consulta_RecyclerView.this).addToRequestQueue(stringRequest);
+    }
+
+
+    private void DialogConfirmacion(){
+        String mensaje = "¿Realmente desea salir?";
+        dialogo = new AlertDialog.Builder(Consulta_RecyclerView.this);
+        dialogo.setIcon(R.drawable.ic_close);
+        dialogo.setTitle("Advertencia");
+        dialogo.setMessage(mensaje);
+        dialogo.setCancelable(false);
+        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo, int id) {
+                finish();
+            }
+        });
+        dialogo.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo, int id) {
+                Toast.makeText(getApplicationContext(), "Operación Cancelada.", Toast.LENGTH_LONG).show();
+            }
+        });
+        dialogo.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_recycler_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_salir){
+            DialogConfirmacion();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
