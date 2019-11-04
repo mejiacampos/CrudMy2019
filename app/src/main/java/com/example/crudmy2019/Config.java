0a744ecc0c8e5a13334f763aae0abd1bbd1cb95b
mejiacampos <marvin.campos2000@gmail.com>
2019-11-04 02:45:41 +0100

    agrege la clase Config

---
+++ app/src/main/java/com/example/crudmy2019/Config.java
@@ -0,0 +1,16 @@
+package com.example.crudmy2019;
+
+public class Config {
+    public static final String urlGuardar = "http://192.168.187.109/sis21b/guardar.php";
+
+    public static final String urlEliminar = "http://192.168.187.109/sis21b/eliminar.php";
+    public static final String urlActualizar = "http://192.168.187.109/sis21b/actualizar.php";
+
+    public static final String urlConsultaApiMySQLi = "http://192.168.187.109/sis21b/Api.php";
+    public static final String urlConsultaApiPDO = "http://192.168.187.109/sis21b/buscarAll.php";
+
+    public static final String urlConsultaCodigo = "http://192.168.187.109/sis21b/buscarArticulosCodigo.php";
+    public static final String urlConsultaDescripcion = "http://192.168.187.109/sis21b/buscarArticulosDescripcion.php";
+    public static final String urlConsultaAllArticulos = "http://192.168.187.109/sis21b/buscarArticulos.php";
+
+}
